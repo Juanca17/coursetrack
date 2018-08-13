563014eb59f39b26c22cbe40b6b3948a77bc2679
Juanca <juancasan17@gmail.com>
2018-08-13 04:45:23 -0500

    create models

--- common/models/student.js
+++ common/models/student.js
@@ -1,5 +1,20 @@
 'use strict';
 
 module.exports = function(Student) {
+  var app = require('../../server/server');
 
+  // make admin role to the new instance
+  Student.observe('after save', async function(ctx) {
+    app.models.Role.find({ name: 'student' }, function(err, results) {
+      results[1].principals.create({
+        principalType: app.models.RoleMapping.USER,
+        principalId: ctx.instance.id
+      }, function(err, principal) {
+        if (err) throw err;
+
+        // console.log('Created principal:', principal);
+      });
+    });
+    return;
+  });
 };
--- server/boot/student-role.js
+++ server/boot/student-role.js
@@ -3,12 +3,12 @@ module.exports = function(app) {
   var User = app.models.user;
   var Role = app.models.Role;
   var RoleMapping = app.models.RoleMapping;
-  var Administrator = app.models.Administrator;
+  var Student = app.models.Student;
 
-  Administrator.find({}, function(err, administrators) {
+  Student.find({}, function(err, students) {
     // create the admin role
     Role.create({
-      name: 'admin'
+      name: 'student'
     }, function(err, role) {
       if (err) throw err;
 
@@ -16,17 +16,15 @@ module.exports = function(app) {
 
       role.principals.create({
         principalType: RoleMapping.USER,
-        principalId: administrators[0].id
+        principalId: students[1].id
       }, function(err, principal) {
         if (err) throw err;
 
         console.log('Created principal:', principal);
       });
     });
+
   });
 
-  Role.find({ name: 'admin' }, function(err, results) {
-    console.log(results[0].id);
-  });
 };
 */
