ca65c41ff0c9a48682491a8bde322943c48de3ce
Juanca <juancasan17@gmail.com>
2018-08-13 08:12:22 -0500

    create models

--- common/models/student.js
+++ common/models/student.js
@@ -17,4 +17,34 @@ module.exports = function(Student) {
     });
     return;
   });
+
+  /**
+   * Register Student to a Course.
+   * @param {string} courseId Course to register the Student into.
+   * @param {Function(Error)} callback
+   */
+
+  Student.register = function(courseId, callback) {
+    var course = courseId;
+    app.models.Registration.create({
+      courseId: course
+    }, function(err, role) {if (err) throw err;});
+
+    callback(null, courseId);
+  };
+
+  // Operation hook to complete registration
+  Student.afterRemote('register', function(ctx, remoteMethodOutput, next) {
+    app.models.Registration.find({
+      where:{courseId: remoteMethodOutput}}, function(err, register) {
+        /*const token = ctx.options && ctx.options.accessToken;
+        const userId = token && token.userId;
+        const user = userId ? 'user#' + userId : '<anonymous>';
+        app.models.Registration.updateAttribute({studentId: user}, function(err, instance) {});*/
+        console.log(ctx.req);
+    });
+    next();
+  });
 };
+
+// 5b713fb901aaa559efdd6377
---
+++ server/boot/register.js
@@ -0,0 +1,10 @@
+/*module.exports = function(app) {
+  var User = app.models.user;
+  var Role = app.models.Role;
+  var RoleMapping = app.models.RoleMapping;
+  var Registration = app.models.Registration;
+
+  Registration.find({where:{courseId: '5b713fb901aaa559efdd6377'}}, function(err, register) {
+    console.log(register);
+  });
+}*/
